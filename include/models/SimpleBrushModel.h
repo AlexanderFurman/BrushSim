a7b00d1f3b8fe1cf4947545ce2364510af0237fc
Alex Furman <alexfurman16@gmail.com>
2024-07-31 12:00:04 +0300

    finishing initial implementation of updateState (need to next break up into sub-routines)

--- include/models/SimpleBrushModel.h
+++ include/models/SimpleBrushModel.h
@@ -16,7 +16,8 @@ struct FootPrint{
     double rho; // secondary angle of rotation of the intersecting ellipse (user defined?)
     double k_u; // modifying factor for u
     double k_v; // modifying factor for v
-    double b;
+    double b; // modifying factor for u, v -- bias
+    Eigen::MatrixXd paintDeposited; // pixels painted by current brush stroke, with percent of paint deposited (value between 0-1)
 };
 
 // This brush model comes from the paper Virtual brush: a model-based synthesis of Chinese calligraphy (Wong et al.)
@@ -26,21 +27,25 @@ class SimpleBrushModel: public IModel{
     const double m_brushRadius; // length of radius of the brush stem
     const double m_brushLength; // length of brush stem to tip of brush hair
     const double m_hairLength; //length of each hair in the brush (note, for now we are assuming each hair is exactly brush_length long)
-    const Pose m_worldFrame; // World frame is defined as the bottom left corner of the paper/canvas. position=[0,0,0], orientation=[0,0,0]
-    const Pose m_paper; // pose of center of the paper, relative to world frame.
+    const Pose m_worldFrame; // World frame is defined as the top left corner of the paper/canvas. position=[0,0,0], orientation=[0,0,0] x is down the page, y is to the right, z is out of the page
+    // const Pose m_paper; // pose of center of the paper, relative to world frame.
     //TODO: If this (below) is too slow, replace using Eigen batch multiplication
     const vector<Eigen::Vector3d> m_hairBase; // Collection of positions of all the bases of each hair of the brush, relative to the brush stem
     const Pose m_brushStemInitialPose; // Initial Pose (position + orientation) of the brush, relative to the brush stem. 
-                                        // We assume brush initially points upwards and is in the bottom left corner, as in theta = [0,0,1] [rad]
+                                        // We assume brush initially points directly downwards and is in the top left corner, with some elevation off the page (position = [0,0,z], orientation = [0,0,0])
+
 
     //Dynamic Variables
     Pose m_brushStemPose; // pose of center of stem of the brush, relative to world frame.
     Twist m_brushStemTwist; // twist of center of stem of the brush, relative to world frame.
     Pose m_brushTipPose; //pose of the tip of the brush
     FootPrint m_footprint; // information about the footprint of the brush
+    Eigen::MatrixXd canvas; // canvas with ink/paint deposited
 
     public:
     void updateState(const ISimStep& simStep) override;
+    void initialize(const IConfig& config) override;
+    const ISimResult& getResult() const override;
 };
 
 #endif // SIMPLEBRUSHMODEL_H
--- src/models/SimpleBrushModel.cpp
+++ src/models/SimpleBrushModel.cpp
@@ -1,6 +1,14 @@
 #include "SimpleBrushModel.h"
 #include "Conversions.h"
 #include <cmath>
+#include <random>
+
+//create random seed for testing
+unsigned int seed = 42;
+std::mt19937 gen(seed);
+std::uniform_real_distribution<> dis(0.0, 1.0);
+
+
 void SimpleBrushModel::updateState(const ISimStep& simStep){
     //Extract const data from simulation step
     m_brushStemPose = simStep.getPose(); // Pose of brush stem
@@ -30,14 +38,14 @@ void SimpleBrushModel::updateState(const ISimStep& simStep){
 
     // ESTIMATE INITIAL VALUES OF v, u. (As if the brush footprint was a perfect circle)
     double brushTipPosition_z = m_brushTipPose.position[2];
-    double u = m_brushRadius / (m_brushLength * (m_brushLength - brushTipPosition_z));
-    double v = u;
+    m_footprint.u = m_brushRadius / (m_brushLength * (m_brushLength - brushTipPosition_z));
+    m_footprint.v = m_footprint.u;
 
     // Modifying factors of v, u -- These are used to deform the footprint into an ellipse when moving the brush on the page
     // In the paper they seem to be numbers generated upon trial and error.
     // Will input constant values for now, but can dafinitely update these values according to speed of brush
-    double k_u = 1.2;
-    double k_v = 1.7;
+    m_footprint.k_u = 1.2;
+    m_footprint.k_v = 1.7;
     
     // ellipse rotation modifying factor -- seemingly used when "extra rotation" is needed in a brushstroke
     // For now, will set a constant value, however it seems that the angle can actually be determined by comparing the
@@ -65,14 +73,20 @@ void SimpleBrushModel::updateState(const ISimStep& simStep){
         double hairPosition_x = hairPosition[0];
         double hairPosition_y = hairPosition[1];
         Eigen::Vector2d hairPositionScaled;
-        hairPositionScaled << hairPosition_x * (v * k_v + b) / m_brushRadius,
-                              hairPosition_y * (u * k_u + b) / m_brushRadius;
+        hairPositionScaled << hairPosition_x * (m_footprint.v * m_footprint.k_v + b) / m_brushRadius,
+                              hairPosition_y * (m_footprint.u * m_footprint.k_u + b) / m_brushRadius;
 
         
         pixel_xy = brushTipPosition_xy + rotationMat * hairPositionScaled;
+
+        double x, y;
+        x = std::round(pixel_xy[0]);
+        y = std::round(pixel_xy[1]);
+
+        // Note: we need to define some kind of ink model, which states firstly how much ink can be used by a brush before it needs to be redipped.
+        // For now however, we will assume it will not run out.
+
+        // Apply a random density (0-1) of ink to pixel (i,j):  (this should probably a more complex ink depositing mdoel, but lets keep this for testing)
+        m_footprint.paintDeposited.coeffRef(x,y) = dis(gen);
     }
-
-
-
-
 };
